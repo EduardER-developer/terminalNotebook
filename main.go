4afcdf2cc365964188def6eb7b2d6d745e3d810b
EduardER-developer <frkd10wot@ya.ru>
2026-02-12 21:01:46 +0300

    add selectRecord

--- main.go
+++ main.go
@@ -7,25 +7,55 @@ import (
 )
 
 type Record struct {
-	Id int `json:"id"`
-	Title string `json:"title"`
-	Description string `json:"description"`
+	Title 		string 	`json:"title"`
+	Description string 	`json:"description"`
 }
 
 func main() {
+
+	var record Record
+	readFile(&record)
+
+	
+	// fmt.Printf("ID: %d, TITLE: %s", record.Id, record.Title)
+
+	data, _ := json.Marshal(record)
+	fmt.Println(string(data))
+	
+
+	for {
+		fmt.Printf("Send 0-create new notebook, else send title record\n")
+		selectRecord := ""
+		fmt.Scan(&selectRecord)
+
+		if selectRecord == "0" {
+			fmt.Printf("You select command - new notebook\n")
+			fmt.Printf("Enter you record title:\n")
+			titleNewRecord := ""
+			fmt.Scan(&titleNewRecord)
+			fmt.Printf("Enter you record description:\n")
+			descriptionNewRecord := ""
+			fmt.Scan(&descriptionNewRecord)
+
+			createRecord()
+		}
+	}
+}
+
+func createRecord()  {
+	
+}
+
+func readFile(record *Record)  {
 	fileData, err := os.ReadFile("data.json")
 	if err != nil {
 		fmt.Println("Ошибка чтения файла", err)
 		return
 	}
 
-	var record Record
-
-	err = json.Unmarshal(fileData, &record)
+	err = json.Unmarshal(fileData, record)
 	if err != nil {
 		fmt.Println("Ошибка парсинга Json", err)
 		return
 	}
-
-	fmt.Printf("ID: %d, TITLE: %s", record.Id, record.Title)
 }
